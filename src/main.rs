6e58c8cfe9f8f5ee237b1dc6cfc0684244ca24da
George Kaplan <george.kaplan@visionistinc.com>
2019-04-08 16:20:55 -0400

    preserve whitespace at beginning of message

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,12 @@ struct App {
     /// The emoji to represent negative space
     #[structopt(short)]
     background: Option<String>,
+    /// Maximum number of characters per line of output
     #[structopt(short, default_value = "10")]
     max_chars: usize,
+    /// Output raw (don't prefix with a dot to preserve negative space)
+    #[structopt(short)]
+    raw_out: bool,
     /// The text to print in emoji
     text: String,
 }
@@ -19,12 +23,18 @@ struct App {
 fn main() {
     let app = App::from_args();
     let moji = slack_moji::Moji::new(app.foreground, app.background, app.max_chars);
-    let txt = moji.draw(&app.text);
+    let mut txt = moji.draw(&app.text);
 
     if txt.len() > 4000 {
         eprint!("{}: ", Yellow.bold().paint("WARNING"));
         eprint!("Output is greater than 4000 characters. ");
         eprintln!("Slack will not let you send it.");
+    }
+
+    if !app.raw_out && txt.starts_with(' ') {
+        unsafe {
+            txt.as_mut_vec()[0] = b'.';
+        }
     }
 
     println!("{}", txt);
