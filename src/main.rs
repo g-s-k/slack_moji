ec6df56b9716bd7134dcb215f4ccd44161bdb297
George Kaplan <george@georgekaplan.xyz>
2020-04-05 16:07:37 -0400

    upgrade to clap beta

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,27 @@
 use ansi_term::Color::Yellow;
-use structopt::StructOpt;
+use clap::Clap;
 
-#[derive(StructOpt)]
-#[structopt(raw(setting = "structopt::clap::AppSettings::ColoredHelp"))]
+#[derive(Clap)]
+#[clap(setting = clap::AppSettings::ColoredHelp)]
 struct App {
     /// The emoji to represent filled-in space
-    #[structopt(short, default_value = "rust")]
+    #[clap(short, default_value = "rust")]
     foreground: String,
     /// The emoji to represent negative space
-    #[structopt(short)]
+    #[clap(short)]
     background: Option<String>,
     /// Maximum number of characters per line of output
-    #[structopt(short, default_value = "10")]
+    #[clap(short, default_value = "10")]
     max_chars: usize,
     /// Output raw (don't prefix with a dot to preserve negative space)
-    #[structopt(short)]
+    #[clap(short)]
     raw_out: bool,
     /// The text to print in emoji
     text: String,
 }
 
 fn main() {
-    let app = App::from_args();
+    let app = App::parse();
     let moji = slack_moji::Moji::new(app.foreground, app.background, app.max_chars);
     let mut txt = moji.draw(&app.text);
 
